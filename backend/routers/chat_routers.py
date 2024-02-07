6b162c8fa7374aec9937776199340383388cddf5
Chromecore <runnotsoslow@outlook.com>
2024-02-07 16:30:32 -0700

    Added base front end functionality

--- backend/routers/chat_routers.py
+++ backend/routers/chat_routers.py
@@ -20,16 +20,19 @@ def get_chats():
         chats=sorted(chats, key=lambda chat: chat.name),
     )
 
+
 @chats_router.get("/{chat_id}", response_model=ChatResponse,
                   description="Get a chat for a given chat id.")
 def get_chat(chat_id: str):
     """Get a chat by id."""
     return ChatResponse(chat=db.get_chat_by_id(chat_id))
 
+
 @chats_router.put("/{chat_id}", response_model=ChatResponse)
 def update_chat(chat_id: str, chat_update: ChatUpdate):
     """Updates a chat by id."""
     return ChatResponse(chat=db.update_chat(chat_id, chat_update))
+
 
 @chats_router.delete("/{chat_id}", 
                      status_code=204,
@@ -37,6 +40,7 @@ def update_chat(chat_id: str, chat_update: ChatUpdate):
 def delete_chat(chat_id: str):
     """Deletes a chat by id."""
     db.delete_chat(chat_id)
+
 
 @chats_router.get("/{chat_id}/messages", response_model=MessageCollection)
 def get_chat_messages(chat_id: str):
@@ -47,6 +51,7 @@ def get_chat_messages(chat_id: str):
         meta={"count": len(messages)},
         messages=sorted(messages, key=lambda message: message.created_at),
     )
+
 
 @chats_router.get("/{chat_id}/users", response_model=UserCollection)
 def get_chat_users(chat_id: str):
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,37 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import './App.css'
+import ChatsPage from './components/ChatsPage';
 
 const queryClient = new QueryClient();
+
+function NotFound() {
+  return <h1>404: not found</h1>;
+}
+
+function ErrorPage() {
+  return (
+    <>
+      <h1>an error has occurred</h1>
+      <p>contact site admin for support</p>
+    </>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>placeholder</h1>} />
+          {/* Main Routs */}
+          <Route path="/" element={<ChatsPage />} />
+          <Route path="/chats" element={<ChatsPage />} />
+          <Route path="/chats/:chatId" element={<ChatsPage />} />
+
+          {/* Errors */}
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="/error/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/error/404" />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
